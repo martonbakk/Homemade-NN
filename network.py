6168a551f4cf49b355585ed85bde795e7a5f5ac4
martonbakk <marci.bakk2004@gmail.com>
2024-08-04 19:02:10 +0200

    test and class

--- network.py
+++ network.py
@@ -12,11 +12,30 @@ class Network:
     def add_layer(self, layer):
         self.layers.append(layer)
 
-    def train(self, inputs, expected_outputs, epochs):
+    def train(self, inputs, expected_outputs, epochs, learning_rate):
+        state=1
         for e in range(epochs):
             error=0
             for output, exoutput in zip(inputs, expected_outputs):
-                 for layer in self.layers:
+                state+=1
+                for layer in self.layers:
                     output = layer.forward(output)
+                error += mse(exoutput, output)
+                gradre = mse_prime(exoutput, output)
+                grad=gradre[:,0].reshape(10, 1)
+                for layer in reversed(self.layers):
+                    grad = layer.backward(grad, learning_rate)
+                print(f"{state/(len(inputs)*epochs)*100}")
+
             error /= len(inputs)
             print('%d/%d, error=%f' % (e+1, epochs, error))
+    
+    def run(self, data, expected_res,options, images):
+        print(len(data[:5]))
+        for i, input_data in enumerate(data[:5]):
+            output = input_data
+            for layer in self.layers:
+                output = layer.forward(output)
+            print(f"A hálózat kimenete {i+1}: {options[np.argmax(output)]} helyes: {options[np.argmax(expected_res[i])]}")
+            plt.imshow(images[i])
+            plt.show()
---
+++ testnn.py
@@ -0,0 +1,46 @@
+from network import Network 
+from dense import Dense
+from tanh import Tanh
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+# Címkék one-hot kódolása
+y_train = to_categorical(y_train, num_classes=10)
+y_test = to_categorical(y_test, num_classes=10)
+
+prep_x_train=[]
+prep_y_train=[]
+prep_x_test=[]
+prep_y_test=[]
+images=[]
+for x, y in zip(x_train, y_train):
+    prep_x_train.append(x.reshape(-1, 1))   # (784, 1)
+    prep_y_train.append(y.reshape(-1, 1))
+
+for x, y in zip(x_test, y_test):
+    images.append(x)
+    prep_x_test.append(x.reshape(-1, 1))   # (784, 1)
+    prep_y_test.append(y)
+
+
+network=Network()
+network.add_layer(Dense(784, 128))
+network.add_layer(Tanh())
+network.add_layer(Dense(128, 10))
+network.add_layer(Tanh())
+
+
+epochs = 2
+learning_rate = 0.1
+res=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+network.train(prep_x_train, prep_y_train,epochs, learning_rate)
+network.run(prep_x_test, prep_y_test, res, images)
