d1606c94ecd0160f222ff49eb98376b43bf38ef9
martonbakk <marci.bakk2004@gmail.com>
2024-08-04 17:27:14 +0200

    Adding functions

--- network.py
+++ network.py
@@ -8,8 +8,15 @@ import matplotlib.pyplot as plt
 class Network:
     def __init__(self):
         self.layers=[]
-        pass
+        
     def add_layer(self, layer):
         self.layers.append(layer)
 
-        
+    def train(self, inputs, expected_outputs, epochs):
+        for e in range(epochs):
+            error=0
+            for output, exoutput in zip(inputs, expected_outputs):
+                 for layer in self.layers:
+                    output = layer.forward(output)
+            error /= len(inputs)
+            print('%d/%d, error=%f' % (e+1, epochs, error))
