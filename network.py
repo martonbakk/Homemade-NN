63633ae58e0d45a3dcd893baa2097b77568e2bb0
martonbakk <marci.bakk2004@gmail.com>
2024-08-04 17:16:48 +0200

    creating the class

--- network.py
+++ network.py
@@ -3,3 +3,13 @@ from dense import Dense
 from tanh import Tanh
 from losses import mse, mse_prime
 import matplotlib.pyplot as plt
+
+
+class Network:
+    def __init__(self):
+        self.layers=[]
+        pass
+    def add_layer(self, layer):
+        self.layers.append(layer)
+
+        
